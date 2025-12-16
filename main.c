ff91bb52538970229de30fd2ddf650ccc7463b57
vorkuta <vorkutavorkutlag@gmail.com>
2025-12-17 00:55:32 +0100

    main: passing matrix as pointer to beginning of matrix with a single double pointer.

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define EPS_ABS 1e-12
+#define EPS_REL 1e-9
+
+void debug_matrix(double * matrix, size_t dimension) {
+    for (size_t col = 0; col < dimension; col++) {
+        printf("\n");
+        for (size_t element_index = 0; element_index < dimension; element_index++) {
+            printf("%lf ", matrix[col * dimension + element_index]);
+        }
+    }
+
+    printf("\n");
+}
 
 int main() {
     size_t dimension;
@@ -10,20 +26,12 @@ int main() {
     
     double matrix[dimension][dimension];
     
-    printf("Enter Matrix by Columns:\n");
+    printf("Enter Matrix:\n");
     for (size_t col = 0; col < dimension; col++) {
         for (size_t element_index = 0; element_index < dimension; element_index++) {
             scanf("%lf", &matrix[col][element_index]);
         }
     }
 
-    printf("Attemping to print.");
-
-    for (size_t col = 0; col < dimension; col++) {
-        printf("\n");
-        for (size_t element_index = 0; element_index < dimension; element_index++) {
-            printf("%lf ", matrix[col][element_index]);
-        }
-    }
-    printf("\n");
+    debug_matrix(matrix[0], dimension);
 }
